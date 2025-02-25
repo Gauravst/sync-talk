42d41b05e996dbd1690c23fcb56b937cce61e102
Gauravst <gauravsinghtangariya1@gmail.com>
2025-02-25 13:53:55 +0530

    fix : 0.0.0.0 host added, frontend host bug fixed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: Number(process.env.VITE_PORT) || 5173,
   },
   resolve: {
